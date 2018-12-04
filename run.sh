3ff9d20110d23e93b0f64818975c193edb9c423f
Philipp Andreas <philipp.andreas@vanzoo.de>
2018-12-04 13:12:16 +0100

    clean shutdown

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ test -e "/data/graphite/conf/storage-aggregation.conf" && echo "storage-aggregat
 test -e "/data/graphite/conf/storage-schemas.conf" && echo "storage-schemas.conf already exists!" || cp /var/lib/graphite/conf/storage-schemas.conf.example /data/graphite/conf/storage-schemas.conf
 test -e "/data/graphite/conf/carbon.conf" && echo "carbon.conf already exists!" || cp /var/lib/graphite/conf/carbon.conf.example /data/graphite/conf/carbon.conf
 
-/var/lib/graphite/bin/carbon-cache.py start --debug
+/var/lib/graphite/bin/carbon-cache.py start --debug &
+PID=$!
+trap 'while kill $PID > /dev/null 2>&1;do sleep 1;done;exit 0' SIGTERM
+while true; do sleep 10; done
